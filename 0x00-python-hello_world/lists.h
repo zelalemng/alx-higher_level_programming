12de163609452add609c4ed657abd19a26ca04f2
zelalemng <zedneg@gmail.com>
2023-08-09 19:43:25 +0000

    header

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,4 +1,4 @@
-#ifndef LISTS_H
+ifndef LISTS_H
 #define LISTS_H
 
 #include <stdlib.h>
@@ -12,8 +12,8 @@
 */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+        int n;
+        struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
