f424fbd47810f9fc182ae906e3d3b9d6a414934b
zelalemng <zedneg@gmail.com>
2023-09-05 21:04:00 +0000

    task_py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+"""Defines a locked class."""
 class LockedClass:
     """A locked class that only lets the user dynamically create the instance
     attribute 'first_name'"""
-    __slots__ = ['first_name']
+    __slots__ = ["first_name"]
