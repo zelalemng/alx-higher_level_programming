f92a30c502d10b26a19b6d39a1c7e16d04582f06
zelalemng <zedneg@gmail.com>
2023-09-11 17:48:53 +0000

    task_0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" attributes and methods of an object """
+def lookup(obj):
+    """ returns the list of available attributes and methods of an object """
+    return(dir(obj))
