e62120d28d6c851b895beb38cdea4e93cb54e53b
zelalemng <zedneg@gmail.com>
2023-09-12 13:04:08 -0700

    task_3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""returns the JSON representation of object (string) """
+import json
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string) """
+    return json.dumps(my_obj)
