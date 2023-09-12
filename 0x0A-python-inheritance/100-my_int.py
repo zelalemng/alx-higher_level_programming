071499fa64cf698c8a3a87564ec527f75e8248af
zelalemng <zedneg@gmail.com>
2023-09-12 10:43:58 -0700

    task_100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Contains the class MyInt"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class MyInt(int):
+    """rebel version of an integer, perfect for opposite day!"""
+    def __new__(cls, *args, **kwargs):
+        """create a new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+    def __eq__(self, other):
+        """what was != is now =="""
+        return int(self) != other
+    def __ne__(self, other):
+        """what was == is now 1="""
+        return int(self) == other
