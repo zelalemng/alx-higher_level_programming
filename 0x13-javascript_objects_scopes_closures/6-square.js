83093c552de0fcd95518609026f757627590fa31
zelalemng <zedneg@gmail.com>
2023-11-14 07:50:50 -0800

    js_objects

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const SquareP = require('./5-square');
+
+const Square extends SquareP {
+	charPrint (c) {
+		if (c === undefined) {
+			c = 'X';
+		}
+		for (let i = 0; i < this.height; i++) {
+			lets s = '';
+			for (let j = 0; j < this.height; i++) {
+				s += c;
+			}
+			console.log(s);
+		}
+	}
+}
+module.exports = Square;
