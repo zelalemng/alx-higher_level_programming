6e7015a5a28b1ad066982a7525f608853ae9a1eb
zelalemng <zedneg@gmail.com>
2023-11-30 11:31:44 -0800

    network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,7 +4,7 @@
 def find_peak(list_of_integers):
     if list_of_integers == []:
         return None
-    size -len(list_of_integers)
+    size = len(list_of_integers)
     if size == 1:
         return list_of_integers[0]
     elif size == 2:
@@ -14,6 +14,6 @@ def find_peak(list_of_integers):
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
     elif peak < list_of_integers[mid - 1]:
-        return find_peak(peak > list_of_integers[:mid])
+        return find_peak(list_of_integers[:mid])
     else:
-        return find_peak(peak > list_of_integers[mid + 1:])
+        return find_peak(list_of_integers[mid + 1:])
