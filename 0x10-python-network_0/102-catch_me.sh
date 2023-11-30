2166068452cc02971471aaf0a86b745d5ea11b76
zelalemng <zedneg@gmail.com>
2023-11-30 10:54:38 -0800

    network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # make a message containing you got me!, in the body of the response
-curl -o /dev/null -sw "You got m!" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -d user_id=98 -H "You got m!" 0.0.0.0:5000/catch_me
