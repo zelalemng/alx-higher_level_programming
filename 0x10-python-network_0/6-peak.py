e0fd53c4e2bb66355482c2203df2065f36dc79ed
zelalemng <zedneg@gmail.com>
2023-11-30 10:07:14 -0800

    network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
+""" Script for finding peak in list of ints6-peak.py, interview prep"""
 
 def find_peak(list_of_integers):
-    if list_of_integers is None or len(List_of_integers) == 0:
-        return None
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-    mid_idx = int(len(list_of_integers) / 2)
-
-    if mid_idx = len(list_of_integers) - 1:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
-                list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
-                    return list_of_integers[mid_idx]
-    else:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
-        else:
-            return list_of_integers[mid_idx - 1]
-    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
-        a_list = list_of_integers[0:mid_idx]
-    else:
-        a_list = list_of_integers[mid_idx + 1:]
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i
