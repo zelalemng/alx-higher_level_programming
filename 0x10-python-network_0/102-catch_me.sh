85a42c759b50e6307eab1f40b88be4870b5970fd
zelalemng <zedneg@gmail.com>
2023-11-30 10:50:57 -0800

    network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# make a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing you got me!, in the body of the response
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+# make a message containing you got me!, in the body of the response
+curl -o /dev/null -sw "You got m!" 0.0.0.0:5000/catch_me
