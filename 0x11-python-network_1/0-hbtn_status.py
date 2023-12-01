db8b6c5161b596f98c6fa2f566d0da9d7ded7225
zelalemng <zedneg@gmail.com>
2023-12-01 12:08:10 -0800

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,8 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as request:
+
         response = request.read()
 
         print("Body response:")
