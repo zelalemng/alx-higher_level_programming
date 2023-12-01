a32e0653da5d53786d6e066ab1a3393a13bd69b6
zelalemng <zedneg@gmail.com>
2023-12-01 10:25:37 -0800

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Fetching URLs with urllib """
+
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-internet.hbtn.io/status') as response:
         html = response.read()
-        print("Body response")
+        print("Body response:")
         print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
                 .format(type(html), html, html.decode('utf-8')))
