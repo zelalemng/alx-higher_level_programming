56e1d637ae18efe4946515199a4d81377f1b9e0a
zelalemng <zedneg@gmail.com>
2023-12-01 09:00:20 -0800

    network_1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,9 +2,10 @@
 """ list 10 commits (from the most recent to oldest) of 
 the respository and user sent in as arguments"""
 
-if __name__ == '__main__':
-    import requests
-    from sys import argv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
     r = requests.get('https://api.github.com/repos/{}/{}/commits'
             .format(argv[2], argv[1]))
     commits = r.json()
