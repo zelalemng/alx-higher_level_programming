59a8bf1681b8023a4177566e0d3372b0ebf45f1e
zelalemng <zedneg@gmail.com>
2023-12-01 12:25:55 -0800

    network

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" This Python script takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter"""
+""" This Python script takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
 
 import requests
 import sys
