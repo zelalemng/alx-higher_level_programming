2e5d9862981fb2b2378f43b1b8eb9e15c8b7cf18
zelalemng <zedneg@gmail.com>
2023-12-01 10:31:20 -0800

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,8 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-internet.hbtn.io/status') as response:
-        html = response.read()
+    with urllib.request.urlopen('https://alx-internet.hbtn.io/status') as request:
+        response = request.read()
         print("Body response:")
         print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
                 .format(type(html), html, html.decode('utf-8')))
