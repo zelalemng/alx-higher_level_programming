5bcb04c03af57ba85059112640f00a4ef9bbc112
zelalemng <zedneg@gmail.com>
2023-12-01 09:52:02 -0800

    network_1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
     email = {'email': sys.argv[2]}
-    data = urlli.parse.urlencode(email).encode('utf-8')
+    data = urllib.parse.urlencode(email).encode('utf-8')
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         resp = response.read()
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,7 +2,7 @@
 """Fetching URLs with Requests library """
 import requests
 
-if __name___ == "__main__":
+if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
     t = r.text
     print("Body response:")
