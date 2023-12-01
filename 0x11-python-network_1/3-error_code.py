99c9f61960e46f002a1c455d366f76e049c1a7f1
zelalemng <zedneg@gmail.com>
2023-12-01 11:48:36 -0800

    network

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)."""
+"""Takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).
+"""
 
 import urllib.parse
 import urllib.request
 import sys
 
-if __name__ == "__mai__":
+if __name__ == "__main__":
     req = urllib.request.Request(sys.argv[1])
+
     try:
         with urllib.request.urlopen(req) as response:
             resp = response.read()
