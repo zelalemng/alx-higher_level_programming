4bef26f38007587f084f110a7b4803be0a90897a
zelalemng <zedneg@gmail.com>
2023-12-01 09:58:46 -0800

    network_1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,6 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-    angument = sys.argv[1]
+    argument = sys.argv[1]
     with urllib.request.urlopen(argument) as response:
-        print(response.hesders["X-Request-Id'])
+        print(response.headers['X-Request-Id'])
