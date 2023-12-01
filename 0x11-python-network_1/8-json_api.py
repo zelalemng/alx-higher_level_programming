c09fc2076f6876c28e8653a8d5b6260525554ca7
zelalemng <zedneg@gmail.com>
2023-12-01 12:29:44 -0800

    network

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,12 +8,14 @@ import sys
 
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
+
     if len(sys.argv) != 1:
         q = sys.argv[1]
     else:
         q = " "
     try:
         req = requests.post(url, data={'q': q}).json()
+
         if len(req) == 0:
             print('No result')
         else:
